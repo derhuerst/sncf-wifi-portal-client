88a5fb356b565db6c57cf5433f62f3eeddddd465
Jannis R <mail@jannisr.de>
2021-04-13 16:15:05 +0200

    make timeout configurable

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ const {fetch} = require('fetch-ponyfill')({
 const endpoint = 'https://wifi.sncf/router/api/'
 const userAgent = 'https://github.com/derhuerst/snvg-wifi-portal-client'
 
-const request = async (route) => {
+const request = async (route, opt = {}) => {
 	if ('string' !== typeof route) throw new Error('route must be a string')
 
 	const res = await fetch(endpoint + route, {
 		mode: 'cors',
 		redirect: 'follow',
 		headers: {'User-Agent': userAgent},
-		timeout: 2500
+		timeout: 2500,
+		...opt,
 	})
 	if (!res.ok) {
 		const err = new Error(res.statusText)
@@ -27,17 +28,17 @@ const request = async (route) => {
 	return await res.json()
 }
 
-const trip = () => request('train/details')
-const tripPolyline = () => request('train/graph')
-const gps = () => request('train/gps')
+const trip = request.bind(null, 'train/details')
+const tripPolyline = request.bind(null, 'train/graph')
+const gps = request.bind(null, 'train/gps')
 
-const coverage = () => request('train/coverage')
+const coverage = request.bind(null, 'train/coverage')
 
-const connectionStatus = () => request('connection/status')
-const connectionStatistics = () => request('connection/statistics')
+const connectionStatus = request.bind(null, 'connection/status')
+const connectionStatistics = request.bind(null, 'connection/statistics')
 
-const bar = () => request('bar/attendance')
-const food = () => request('food/catalog')
+const bar = request.bind(null, 'bar/attendance')
+const food = request.bind(null, 'food/catalog')
 
 module.exports = {
 	trip,
